61c7ea3120852226ae418fe4ac2e72a6573cab33
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2018-12-25 20:33:17 -0200

    Footer

---
+++ client/src/common/template/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2018
+      <a href="https://github.com/arthurgrigoletto" target="_blank">
+         Arthur Grigoletto
+      </a>
+    </strong>
+  </footer>
+);
--- client/src/main/app.jsx
+++ client/src/main/app.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 
 import Header from '../common/template/Header';
 import Sidebar from '../common/template/Sidebar';
+import Footer from '../common/template/Footer';
 
 export default props => (
   <div className="wrapper">
     <Header />
     <Sidebar />
+    <div className="content-wrapper">
+      <h1>Conteudo</h1>
+    </div>
+    <Footer />
   </div>
 );
