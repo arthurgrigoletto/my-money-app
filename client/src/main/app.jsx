c372c966b8b3b99b161a1db6bce143e7b3f35fc7
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2018-12-25 23:48:41 -0200

    Navegacao com o React-router

--- client/src/main/app.jsx
+++ client/src/main/app.jsx
@@ -4,13 +4,14 @@ import React from 'react';
 import Header from '../common/template/Header';
 import Sidebar from '../common/template/Sidebar';
 import Footer from '../common/template/Footer';
+import Routes from './routes';
 
 export default props => (
   <div className="wrapper">
     <Header />
     <Sidebar />
     <div className="content-wrapper">
-      <h1>Conteudo</h1>
+      <Routes />
     </div>
     <Footer />
   </div>
